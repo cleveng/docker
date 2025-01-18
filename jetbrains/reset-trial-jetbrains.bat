244dd540054dfd398e169b8dc163dfe3071c07b1
cleveng <cleveng@gmail.com>
2025-01-18 15:34:20 +0800

    perf: add reset-trial-jetbrains.sh

--- jetbrains/reset-trial-jetbrains.bat
+++ jetbrains/reset-trial-jetbrains.bat
@@ -1,5 +1,5 @@
 REM Delete eval folder with licence key and options.xml which contains a reference to it
-for %%I in ("WebStorm", "IntelliJ", "CLion", "Rider", "GoLand", "PhpStorm", "Resharper", "PyCharm") do (
+for %%I in ("IntelliJ", "WebStorm", "CLion", "Rider", "GoLand", "PhpStorm", "Resharper", "PyCharm") do (
     for /d %%a in ("%USERPROFILE%\.%%I*") do (
         rd /s /q "%%a/config/eval"
         del /q "%%a\config\options\other.xml"
